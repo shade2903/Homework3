f53a8742b1a8ec78167f21f480f524e38290b458
shade2903 <78316020+shade2903@users.noreply.github.com>
2021-04-13 17:59:38 +0300

    Add files via upload

--- src/com/company/CoffeeMachine.java
+++ src/com/company/CoffeeMachine.java
@@ -1,13 +1,14 @@
 package com.company;
 
 public abstract class CoffeeMachine {
-    double waterTank;
-    double waterCoffee;
-    String panelNavigation;
+    public double waterTank;
+    public double waterCoffee;
+    public String panelNavigation;
 
     public CoffeeMachine(double waterTank, double waterCoffee, String panelNavigation) {
         this.waterTank = waterTank;
         this.waterCoffee = waterCoffee;
         this.panelNavigation = panelNavigation;
     }
+    public abstract void makeCofee();
 }
---
+++ src/com/company/ProfessionalCoffeeMachine.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class ProfessionalCoffeeMachine extends CoffeeMachine   {
+    @Override
+    public void makeCofee(){
+        System.out.println("Ваш кофе готов");
+    }
+
+
+}
